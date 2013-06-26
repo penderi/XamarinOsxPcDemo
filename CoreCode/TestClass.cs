c8a86ac9f095e497eb70fb522c08e85c39b84cb3
Ian Pender <ian.pender@huddle.com>
2013-06-26 17:46:12 +0100

    First - VS

---
+++ CoreCode/TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using NUnit.Framework;
+
+namespace CoreCode
+{
+    [TestFixture]
+    public class TestClass
+    {
+        private bool fired;
+
+        [Test]
+        public void ShouldNoticeFileChange()
+        {
+            DateTime start = DateTime.Now;
+   
+            var fsw = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            fsw.Changed += FswOnChanged;
+            fsw.EnableRaisingEvents = true;
+
+            while (!fired && (DateTime.Now - start).Seconds < 5)
+            {
+                Thread.Sleep(500);                
+            }
+
+            Assert.That(fired, Is.True);
+        }
+
+        private void FswOnChanged(object sender, FileSystemEventArgs fileSystemEventArgs)
+        {
+            fired = true;
+        }
+    }
+
+    //new class lib 
+    //added to location
+    //nuget nunit
+
+}
